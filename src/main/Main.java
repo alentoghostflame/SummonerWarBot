61b36442d6afdf7ef7771573aa1f4835ed1163bb
alento <alento@localhost>
2018-03-15 10:17:53 -0500

    Cleaned up Main, removed old code.

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,27 +13,6 @@ public class Main {
 		
 		ImageArrayMaker.Main();
 		
-		
-		
-		
-		
-		/*
-		try {
-			AdCheck();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
 		System.out.println("End!");
-	}
-	
-	static void AdCheck() throws IOException {
-		Commands Commands = new Commands();
-		System.out.println("Checking for Ads!");
-		while (Commands.ColorCompare(1200, 1300, 0))
-			Commands.BasicCommand("adb shell input tap 1200 1300");
-		while (Commands.ColorCompare(1850, 500, 1)) {
-			Commands.BasicCommand("adb shell input tap 1850 500");
-		}
 	}
 }
