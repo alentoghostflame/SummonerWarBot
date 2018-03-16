5f53009a2bd2b72c19342f56f206c71c8f949e2c
alexa <alexa@Vinyl-Scratch.mshome.net>
2018-03-15 22:57:07 -0500

    Added reference image capability, added comments.

--- src/main/ArrayHolder.java
+++ src/main/ArrayHolder.java
@@ -1,6 +1,7 @@
 package main;
 
 public class ArrayHolder {
+	//Need complete rework to act as shortcuts to places where UI elements are.
 	public static final int[][] COORDS = {
 			//Community Button
 			{2180, 1300},
@@ -8,17 +9,20 @@ public class ArrayHolder {
 			{330, 250},
 			//Friends 1-5
 			//{2180, 620}
-			
 	};
-	public static final String[] HEXCOLOR = {
+	//Depricated, deleting when Commands.ColorCompare is redone.
+	public static final String[][] HEXCOLOR = {
 			//Brownish Yellow Close Button, 1200 1300
-			"#d6b153",
+			{"#d6b153"},
 			//Yellowish Brown Collect Button, 1850 500
-			"#c19a5c",
-			"test"
+			{"#c19a5c"},
+			{"test"}
 	};
 	
+	//Holds all the hex color values of an image, exclusively for screen.png.
 	public static String[][] IMAGEARRAY = new String[ImageArrayMaker.Width()][ImageArrayMaker.Height()];
+	//Holds all the hex color values of an image, used and replaced by all reference images.
+	public static String[][] REFERENCEIMAGEARRAY = new String[ImageArrayMaker.Width()][2];
 			
 }
 	
