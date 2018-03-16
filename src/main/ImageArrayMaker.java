9bbaee4c66fd41587bc7a1ebeec9c497e4d9ed17
alexa <alexa@Vinyl-Scratch.mshome.net>
2018-03-15 22:59:25 -0500

    Added UpdateScreen, UpdateReference, changed Main to accept width/height

--- src/main/ImageArrayMaker.java
+++ src/main/ImageArrayMaker.java
@@ -11,7 +11,7 @@ import javax.imageio.ImageIO;
 
 
 public class ImageArrayMaker {
-	
+	//Method to figure out the Width of screen.png, and only screen.png.
 	public static final int Width() {
 		try {
 			File file = new File("screen.png");
@@ -23,6 +23,7 @@ public class ImageArrayMaker {
 		}
 		return -1;
 	}
+	//Method to figure out the Height of screen.png, and only screen.png.
 	public static final int Height() {
 		try {
 			File file = new File("screen.png");
@@ -34,25 +35,23 @@ public class ImageArrayMaker {
 		}
 		return -1;
 	}
-	public void WidthHeightChecker() {
-		
-	}
-	public int Main() {
+	//Main method for uploading an image to an Array.
+	public static int Main(String inputimage, String array, int width, int height) {
 		System.out.println("Starting ImageArrayMaker!");
-		Commands Commands = new Commands();
-		Commands.ScreenShot();
 		//Log Array to file for debug
 		BufferedWriter outputWriter = null;
-		//Get Width and Height for this entire function, as well as execute those methods for the
-		//IMAGEARRAY.
-		int width = ImageArrayMaker.Width();
-		int height = ImageArrayMaker.Height();
-		
 		try {
-			//Read the file "screen.png"
-			BufferedImage image = ImageIO.read(new File("screen.png"));
+			BufferedImage image;
+			//If reading screen.png, use method to view files outside the Jar.
+			if (inputimage == "screen.png")
+				image = ImageIO.read(new File(inputimage));
+			else //if reading anything else, use method to view files inside the Jar.
+				image = ImageIO.read(ImageArrayMaker.class.getResource(inputimage));
 			//Log Array to file for debug. Either creates new, or overwrites old log.
-			outputWriter = new BufferedWriter(new FileWriter("ArrayLog.txt"));
+			if (array == "IMAGEARRAY")
+				outputWriter = new BufferedWriter(new FileWriter("ArrayLog.txt"));
+			else
+				outputWriter = new BufferedWriter(new FileWriter("ReferenceArrayLog.txt"));
 			
 			//For the Height of the Image...
 			for (int y = 0; y < height; y++) {
@@ -63,16 +62,23 @@ public class ImageArrayMaker {
 					if (hexColor.length() < 6) {
 					    hexColor = "000000".substring(0, 6 - hexColor.length()) + hexColor;
 					}
-					//Put the hexColor into IMAGEARRAY at specific coordinates
-					ArrayHolder.IMAGEARRAY[x][y] = hexColor;
+					//Put the hexColor into IMAGEARRAY at specific coordinates.
+					//If inserting into main image array, insert into the main image array.
+					if (array == "IMAGEARRAY")
+						ArrayHolder.IMAGEARRAY[x][y] = hexColor;
+					else //if inserting into anything else, insert into the reference image array.
+						ArrayHolder.REFERENCEIMAGEARRAY[x][y] = hexColor;
 					//Print X, Y, and Hex for Debug Purposes. Slows down program considerably.
 					//System.out.println("X: " + x + " Y: " + y + " Hex: " + hexColor);
 					//Log to file for Debug Purposes. Slows down program slightly.
-					outputWriter.write(ArrayHolder.IMAGEARRAY[x][y] + " ");
+					if (array == "IMAGEARRAY")
+						outputWriter.write(ArrayHolder.IMAGEARRAY[x][y] + " ");
+					else
+						outputWriter.write(ArrayHolder.REFERENCEIMAGEARRAY[x][y] + " ");
 				}
 				//Print Y for Debug Reasons, not to be used with Line 70. Slows down program slightly.
 				//System.out.println("Y: " + y);
-				//Go to the next line in ArrayLog.txt
+				//Go to the next line in ArrayLog.txt.
 				outputWriter.newLine();
 			}
 			//Finish up ArrayLog.txt
@@ -84,5 +90,30 @@ public class ImageArrayMaker {
 		}	
 		return 0;
 	}
-
+	
+	public static int UpdateScreen() {
+		//This method is for an easy command to update the screen.
+		Commands Commands = new Commands();
+		Commands.ScreenShot();
+		String inputimage = "screen.png";
+		int width = ImageArrayMaker.Width();
+		int height = ImageArrayMaker.Height();
+		ImageArrayMaker.Main(inputimage, "IMAGEARRAY", width, height);
+		return 0;
+	}
+	public int UpdateReference(String referenceimage) {
+		//This method is for an easy command to change what reference image is being compared to the Screen.
+		String imageloc = "/images/" + referenceimage;
+		System.out.println(imageloc);
+		try {
+			BufferedImage image;
+			image = ImageIO.read(getClass().getResource(imageloc));
+			int width = image.getWidth();
+			int height = image.getHeight();
+			ImageArrayMaker.Main(imageloc, "REFERENCEIMAGEARRAY", width, height);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
