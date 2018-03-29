07b17adba7d7c68d32c93fd1069903f972db22c7
alento <alento@localhost>
2018-03-29 12:23:21 -0500

    Added images to arrays, WIP

--- src/main/ArrayHolder.java
+++ src/main/ArrayHolder.java
@@ -30,13 +30,28 @@ public class ArrayHolder {
 	//than the find where to perform actions.
 	//Examples include the Guild tab, Monthly Event popup, Weekly Arena Award popup, ETC.
 	public static final String[] REFERENCEIMAGETITLES = {
+			//Logo's that appear Pre-Login.
+			"SummonersWarLogo.png", //The main logo that appears right before login.
+			//Logo's that appear in the Hub World.
+			"HubWorldWeeklyArenaRewardLogo.png", //Appears Weekly, if rewards are gained from the arena.
+			"HubWorldMonthlyEventLogo.png" //Appears on first login a day.
+			//Logo's that appear in the Community Menu.
 			
 	};
 	//Images in this array are meant to find where to perform an action, rather than find out
 	//exactly what location the screen is at in Summoners War.
 	//Examples include the X button, Collect buttons, Battle buttons, ETC.
 	public static final String[] REFERENCEIMAGEBUTTONS = {
-			
+			//Buttons that appear in the Hub World.
+			"HubWorldBattleButton.png",
+			"HubWorldMonsterButton.png",
+			"HubWorldChallengeButton.png",
+			"HubWorldCommunityButton.png",
+			"HubWorldShopButton.png",
+			//Buttons that appear in the Community Menu.
+			"CommunityFriendList.png", //Tab that you press to switch between Friend List and Guild.
+			"CommunityGuild.png", //Tab that you press to switch between Guild and Friend List.
+			"Community"
 	};
 	
 	//Holds all the hex color values of an image, exclusively for screen.png.
